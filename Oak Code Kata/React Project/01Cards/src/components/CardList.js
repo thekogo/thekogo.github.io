dbabf624cd1234dae2bc18751b72e8f32174a01e
thekogo <thekogo@gmail.com>
2020-11-02 20:19:36 +0700

    update

--- Oak Code Kata/React Project/01Cards/src/components/CardList.js
+++ Oak Code Kata/React Project/01Cards/src/components/CardList.js
@@ -2,14 +2,44 @@ import React from 'react'
 import Card from './Card'
 
 export default function CardList() {
+
+    const data = [
+        {
+            img: 'https://via.placeholder.com/600x250.png',
+            header: 'Test Card 1',
+            des: 'this is test1'
+        },
+        {
+            img: 'https://via.placeholder.com/600x250.png',
+            header: 'Test Card 1',
+            des: 'this is test2'
+        },
+        {
+            img: 'https://via.placeholder.com/600x250.png',
+            header: 'Test Card 1',
+            des: 'this is test3'
+        },
+        {
+            img: 'https://via.placeholder.com/600x250.png',
+            header: 'Test Card 1',
+            des: 'this is test4'
+        },
+        {
+            img: 'https://via.placeholder.com/600x250.png',
+            header: 'Test Card 1',
+            des: 'this is test'
+        },
+        {
+            img: 'https://via.placeholder.com/600x250.png',
+            header: 'Test Card 1',
+            des: 'this is test',
+            price: 100,
+            quality: 10
+        },
+    ]
     return (
         <div className="card-list">
-            <Card img="https://via.placeholder.com/600x250.png" header="Test Card 1" des="this a test for making card in codecamp#6 project" />
-            <Card img="https://via.placeholder.com/600x250.png" header="Test Card 1" des="this a test for making card in codecamp#6 project" />
-            <Card img="https://via.placeholder.com/600x250.png" header="Test Card 1" des="this a test for making card in codecamp#6 project" />
-            <Card img="https://via.placeholder.com/600x250.png" header="Test Card 1" des="this a test for making card in codecamp#6 project" />
-            <Card img="https://via.placeholder.com/600x250.png" header="Test Card 1" des="this a test for making card in codecamp#6 project" />
-            <Card img="https://via.placeholder.com/600x250.png" header="Test Card 1" des="this a test for making card in codecamp#6 project" />
+            {data.map(item => <Card header={item.header} img={item.img} des={item.des} />)}
         </div>
     )
 }
